0c9fe81d7b232a3180df8c894251230f45e75424
Emily Bennett <emilybennett93@hotmail.com>
2023-12-01 14:35:29 +0000

    feat: part 2 solve and part 1 refactor

--- 1/code.ts
+++ 1/code.ts
@@ -12,15 +12,9 @@ const findFirstAndLastDigit: FindFirstAndLastDigit = (filename) => {
     const numRegex = /\d/;
     let numString: string = "";
 
-    // loop forward
-    for (let i = 0; i < line.length; i++) {
-      const currChar: string = line[i];
-      if (numRegex.test(currChar)) {
-        numString += currChar;
-        break;
-      }
-    }
-    // loop backward
+    const firstMatch = line.match(numRegex)?.[0];
+    numString += firstMatch;
+
     for (let i = line.length - 1; i >= 0; i--) {
       const currChar: string = line[i];
       if (numRegex.test(currChar)) {
@@ -39,3 +33,51 @@ const findFirstAndLastDigit: FindFirstAndLastDigit = (filename) => {
 };
 
 console.log(findFirstAndLastDigit("./input1.txt"));
+
+const findFirstAndLastNumber: FindFirstAndLastDigit = (filename) => {
+  const fileContents = fs.readFileSync(filename, "utf8");
+
+  const splitContents = fileContents.split("\n");
+  const numRef = {
+    one: "1",
+    two: "2",
+    three: "3",
+    four: "4",
+    five: "5",
+    six: "6",
+    seven: "7",
+    eight: "8",
+    nine: "9",
+    1: "1",
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+  };
+
+  let numTotal = 0;
+  splitContents.forEach((line: string) => {
+    const numRegex = /(\d|one|two|three|four|five|six|seven|eight|nine)/;
+
+    const firstMatch = line.match(numRegex)?.[0];
+    let numString = firstMatch ? numRef[firstMatch] : "";
+
+    for (let i = line.length - 1; i >= 0; i--) {
+      const subStr = line.substring(i);
+      const match = subStr.match(numRegex);
+      if (match) {
+        numString += numRef[match[0]];
+        break;
+      }
+    }
+    numTotal += parseInt(numString);
+  });
+
+  return numTotal;
+};
+
+console.log(findFirstAndLastNumber("./input1.txt"));
